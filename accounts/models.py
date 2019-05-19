ac2dc0a301a917562f324dc9d5c97dba3fc3368e
bill <tuntunkimpo@naver.com>
2019-05-19 23:46:02 +0900

    insaedu first commit

---
+++ accounts/models.py
@@ -0,0 +1,75 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+
+def user_path(instance, filename):
+    from random import choice
+    import string
+    arr = [choice(string.ascii_letters) for _ in range(8)]
+    pid = ''.join(arr)
+    extension = filename.split('.')[-1]
+    return 'accounts/{}/{}.{}'.format(instance.user.username, pid, extension)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    nickname = models.CharField('닉네임', max_length=30, unique=True)
+    follow_set = models.ManyToManyField('self',
+                                        blank=True,
+                                        through='Relation',
+                                        symmetrical=False, )
+    picture = ProcessedImageField(upload_to=user_path,
+                                  processors=[ResizeToFill(150, 150)],
+                                  format='JPEG',
+                                  options={'quality': 90},
+                                  blank=True,
+                                  )
+    about = models.CharField(max_length=150, blank=True)
+    GENDER_CHOICES = (
+        ('선택 안 함', '선택 안 함'),
+        ('여성', '여성'),
+        ('남성', '남성'),
+    )
+    gender = models.CharField('성별(선택사항)', max_length=10, choices=GENDER_CHOICES, default='N')
+
+    def __str__(self):
+        return self.nickname
+
+    @property
+    def get_follower(self):
+        return [i.from_user for i in self.follower_user.all()]
+
+    @property
+    def get_following(self):
+        return [i.to_user for i in self.follow_user.all()]
+
+    @property
+    def follower_count(self):
+        return len(self.get_follower)
+
+    @property
+    def following_count(self):
+        return len(self.get_following)
+
+    def is_follower(self, user):
+        return user in self.get_follower
+
+    def is_following(self, user):
+        return user in self.get_following
+
+
+class Relation(models.Model):
+    from_user = models.ForeignKey(Profile, related_name='follow_user', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(Profile, related_name='follower_user', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} -> {}".format(self.from_user, self.to_user)
+
+    class Meta:
+        unique_together = (
+            ('from_user', 'to_user')
+        )
---
+++ accounts/views.py
@@ -0,0 +1,102 @@
+import json
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.conf import settings
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
+from django.contrib.auth.views import LoginView as auth_login
+from allauth.socialaccount.models import SocialApp
+from allauth.socialaccount.templatetags.socialaccount import get_providers
+from .forms import SignupForm, ProfileForm
+from .models import Profile, Relation
+
+
+def login(request):
+    providers = []
+    for provider in get_providers():  # settings/INSTALLED_APPS 내에서 활성화된 목록
+        # social_app속성은 provider에는 없는 속성입니다.
+        try:
+            # 실제 provider 별 Client id/secret 이 등록이 되어 있는가?
+            provider.social_app = SocialApp.objects.get(provider=provider.id, sites=settings.SITE_ID)
+        except SocialApp.DoesNotExist:
+            provider.social_app = None
+        providers.append(provider)
+
+    return auth_login(request,
+                      template_name='accounts/login.html',
+                      extra_context={'providers': providers})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST, request.FILES)
+        if form.is_valid():  # clean_<필드명> 메소드 호출
+            user = form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup.html', {
+        'form': form,
+    })
+
+
+@login_required
+def password_change(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, '비밀번호가 정상적으로 변경되었습니다.')
+            return redirect('post:my_post_list', request.user.username)
+        else:
+            messages.error(request, '오류가 발생하였습니다.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/password_change.html', {
+        'form': form,
+    })
+
+
+@login_required
+def account_change(request):
+    profile = get_object_or_404(Profile, pk=request.user.profile.id)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save()
+            messages.success(request, '회원정보가 정상적으로 변경되었습니다.')
+            return redirect('post:my_post_list', request.user.username)
+        else:
+            messages.error(request, '오류가 발생하였습니다.')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'accounts/account_change.html', {
+        'form': form,
+    })
+
+
+@login_required
+@require_POST
+def follow(request):
+    from_user = request.user.profile
+    pk = request.POST.get('pk')
+    to_user = get_object_or_404(Profile, pk=pk)
+    relation, created = Relation.objects.get_or_create(from_user=from_user, to_user=to_user)
+
+    if created:
+        message = '팔로우 시작!'
+        status = 1
+    else:
+        relation.delete()
+        message = '팔로우 취소'
+        status = 0
+
+    context = {
+        'message': message,
+        'status': status,
+    }
+    return HttpResponse(json.dumps(context), content_type="application/json")
